3dc03f778483d5c74f6c696024fd1829b55703de
root <riyaz.a@tailoredtech.in>
2016-12-24 13:26:59 +0530

    minor changes

--- app/src/main/java/com/pachouri/technovanzahackathon/activity/MainActivity.java
+++ app/src/main/java/com/pachouri/technovanzahackathon/activity/MainActivity.java
@@ -1,15 +1,46 @@
 package com.pachouri.technovanzahackathon.activity;
 
+import android.graphics.Color;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 import com.pachouri.technovanzahackathon.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+
+        initViews();
+    }
+
+    private void initViews() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Technovanza Hackathon");
+        toolbar.setTitleTextColor(Color.WHITE);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case android.R.id.home:
+                drawerLayout.openDrawer(Gravity.LEFT);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
