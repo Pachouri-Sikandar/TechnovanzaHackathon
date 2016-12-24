a888be1ab67fa9b6e2322ce95b9cb8883bbbbeb4
Pachouri-Sikandar <ankitpachouri0891@gmail.com>
2016-12-24 12:38:12 +0530

    Created packages basic architecure

---
+++ app/src/main/java/com/pachouri/technovanzahackathon/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.pachouri.technovanzahackathon;
+
+import android.app.Application;
+
+/**
+ * Created by ankit on 12/24/16.
+ */
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
